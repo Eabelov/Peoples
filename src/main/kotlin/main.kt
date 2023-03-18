58fcc4ea3f50767b2ec617c92540fb5bb9271faa
eabelov <egor.b@mail.ru>
2023-03-18 13:20:53 +0300

    Added exceptions 11, 111, 211 etc.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,16 @@
 fun main() {
-    val likes = 11
-    val result = if (((likes % 10) == 1) || likes == 1) "Понравилось $likes человеку" else "Понравилось $likes людям"
-    println(result)
+    val likes = 1
+    if (likes < 100) {
+        if (likes == 1 || (likes % 10) == 1 && likes != 11){
+            println("Понравилось $likes человеку")
+        } else {
+            println("Понравилось $likes людям")
+        }
+    } else {
+        if ((likes % 100) == 11) {
+            println("Понравилось $likes людям")
+        } else {
+            println("Понравилось $likes человеку")
+        }
+    }
 }
